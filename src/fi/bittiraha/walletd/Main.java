a0a991796bd78f8acf99ec6210c42822fed528af
Joel Kaartinen <jkaartinen@prasos.fi>
2015-06-03 22:43:51 +0300

    Move WalletRPC class into it's own file and rename Test to Main

---
+++ src/fi/bittiraha/walletd/Main.java
@@ -0,0 +1,12 @@
+package fi.bittiraha.walletd;
+
+import org.bitcoinj.params.TestNet3Params;
+import org.bitcoinj.params.MainNetParams;
+import fi.bittiraha.walletd.WalletRPC;
+
+public class Main {
+  public static void main(String[] args) throws Exception {
+    (new WalletRPC(18332,"testnet",TestNet3Params.get())).start();
+    (new WalletRPC(8332,"mainnet",MainNetParams.get())).start();
+  }
+}
